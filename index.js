075144c80c042d61ec77ee42229fe1db735ba452
Sean Kang <pwrkr1@gmail.com>
2012-04-24 08:33:27 +0000

    Added index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('index');
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
